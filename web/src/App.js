2545f5199609a21db1c9dd2b806d5d2a3c52c0ac
Adam Siemer <14990007+abcmer@users.noreply.github.com>
2019-04-14 05:54:52 -0500

    add more fortunes.

--- web/src/App.js
+++ web/src/App.js
@@ -8,7 +8,10 @@ class App extends Component {
     this.state = {
       fortunes: [
         "So toss away stuff you don't need in the end, but keep what's important and know who's your friend",
-        "Don't want to be a climber reaching for the top. Don't want to be anything where I don't know when to stop."
+        "Don't want to be a climber reaching for the top. Don't want to be anything where I don't know when to stop.",
+        "You don't have to be faster than the bear, you just have to be faster than the slowest guy.",
+        "It's easy sometimes when you just coast along, but like it or not something always seems to go wrong",
+        "The rest of your lifetime devoid of a care, if you keep your eyes open you may find yourself there"
       ],
       currentFortune: "Click the cookie, read your fortune."
     };
